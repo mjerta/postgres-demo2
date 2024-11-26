1fc0674168cba56a08f48fdebf8fe2ccdc7c54b6
mjerta <maarten.postma1000@gmail.com>
2024-11-26 13:58:32 +0100

    created a small test

--- src/main/java/nl/mpdev/postgres_demo2/controller/ProductController.java
+++ src/main/java/nl/mpdev/postgres_demo2/controller/ProductController.java
@@ -1,10 +1,13 @@
 package nl.mpdev.postgres_demo2.controller;
 
+import nl.mpdev.postgres_demo2.model.Product;
 import nl.mpdev.postgres_demo2.repository.ProductRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -17,7 +20,14 @@ public class ProductController {
   }
 
   @GetMapping
-  public ResponseEntity geAllProducts() {
+  public ResponseEntity<List<Product>> geAllProducts() {
     return ResponseEntity.ok(this.productRepository.findAll());
+  }
+
+  @PostMapping
+  public ResponseEntity<Product> addNewProduct(@RequestBody Product product) {
+    var test = productRepository.save(product);
+    URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentRequest().path("/" + test.getId()).toUriString());
+    return ResponseEntity.created(uri).body(test);
   }
 }
--- src/main/java/nl/mpdev/postgres_demo2/model/Product.java
+++ src/main/java/nl/mpdev/postgres_demo2/model/Product.java
@@ -9,11 +9,11 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
+@Entity(name = "products")
 public class Product {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
   private String description;
